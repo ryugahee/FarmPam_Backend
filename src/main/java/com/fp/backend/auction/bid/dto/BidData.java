33f79b8aedf6c09ff0dd1e95ffa0fa4bc9c164da
KKchan1119 <kkchan1119@gmail.com>
2023-12-14 03:12:40 +0900

    merge:

--- src/main/java/com/fp/backend/auction/bid/dto/BidData.java
+++ src/main/java/com/fp/backend/auction/bid/dto/BidData.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 public class BidData {
     private String userName;
     private String bidPrice;
+    private long bidTime;
 
-    public BidData(String userName, String bidPrice){
+    public BidData(String userName, String bidPrice, long bidTime){
         this.userName = userName;
         this.bidPrice = bidPrice;
+        this.bidTime = bidTime;
     }
 
 }
