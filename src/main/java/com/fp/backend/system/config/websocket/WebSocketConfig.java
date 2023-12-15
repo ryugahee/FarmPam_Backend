23249d8a3bda337fd4ee0acc0893a8251fe1ead2
KKchan1119 <kkchan1119@gmail.com>
2023-12-15 22:06:29 +0900

    feature: websocket receive BidId

--- src/main/java/com/fp/backend/system/config/websocket/WebSocketConfig.java
+++ src/main/java/com/fp/backend/system/config/websocket/WebSocketConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
-        registry.enableSimpleBroker("/bidList", "/bidPrice", "/send");
+        registry.enableSimpleBroker("/bidList", "/bidPrice", "/send", "/bidPost");
     }
 
     @Override
